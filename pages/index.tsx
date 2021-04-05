c345683ee8eb161ac780f7b076b30e4bb0d53f6f
Nitesh Singh <niteshsingh1357@gmail.com>
2021-04-05 23:10:43 +0545

    transform to Typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,17 @@ import Link from "next/link";
 import Date from "../components/date";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import { GetStaticProps } from 'next'
 
 import { getSortedPostsData } from "../lib/posts";
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
-
-export default function Home({ allPostsData }) {
+export default function Home({ allPostsData }: {
+  allPostsData: {
+    date: string,
+    title: string,
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
       <Head>
@@ -48,4 +46,13 @@ export default function Home({ allPostsData }) {
       </section>
     </Layout>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
